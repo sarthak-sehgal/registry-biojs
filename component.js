085be147c3a7387f554755f292a6ce1536aafa82
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-03-15 12:10:10 +0530

    component.js static API call test

--- component.js
+++ component.js
@@ -1,1 +1,5 @@
-alert(window.localStorage.getItem("component"));
+var  url_name = window.localStorage.getItem("component");
+
+$.getJSON( "http://139.59.93.32/api/datatest/details/"+url_name, function( data ) {
+	alert(data.details);
+});
