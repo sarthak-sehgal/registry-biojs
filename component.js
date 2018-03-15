fb905c40c235f49ee63bc376ba509240ff618f83
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-03-15 12:53:25 +0530

    API call for component to backend

--- component.js
+++ component.js
@@ -1,5 +1,24 @@
-var  url_name = window.localStorage.getItem("component");
+function init()
+{
+	var  url_name = window.localStorage.getItem("component");
 
-$.getJSON( "http://139.59.93.32/api/datatest/details/"+url_name, function( data ) {
-	alert(data.details);
-});
+	$.getJSON( "http://139.59.93.32/api/datatest/details/"+url_name, function( data ) {
+		var visualization = data.visualizations;
+		var details = data.details;
+		var componentContainer = document.getElementById("component-container");
+		var componentName = document.getElementById("component-name");
+
+		function getTags() {
+			var tagsCode='';
+			for(i=0; i<details.tags.length; i++)
+			{
+				tagsCode += '<span class="component-tag">'+details.tags[i]+'</span>';
+			}
+			return tagsCode;
+		}
+
+		componentName.innerHTML += details.name;
+		componentContainer.innerHTML += '<div class="componentInfo">'+details.short_description+'</div><div class="componentNpm componentElement"><code>npm install '+url_name+'</code></div><div class="componentVisualize componentElement" style="padding: 0"><div class="component-heading">Visualization</div><div class="component-content"></div></div><div class="componentTags componentElement" style="padding: 0"><div class="component-heading">Tags</div><div class="component-content">'+getTags()+'</div></div><div class="componentSocial componentElement" style="padding: 0"><div class="component-heading">Social</div><div class="component-content">Call GitHub API</div></div><div class="componentStats componentElement" style="padding: 0"><div class="component-heading">Stats</div><div class="component-content">Call GitHub API</div></div><div class="componentContirbutors componentElement" style="padding: 0"><div class="component-heading">Contributors</div><div class="component-content">Call GitHub API</div></div><div class="componentLegal componentElement" style="padding: 0"><div class="component-heading">Legal</div><div class="component-content">Call GitHub API</div></div>';
+	});
+}
+init();
--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-var most_recent_time = document.getElementsByClassName("most_recent_time");
-var most_recent_name = document.getElementsByClassName("most_recent_name");
-var most_dl_name = document.getElementsByClassName("most_dl_name");
-var most_dl_no = document.getElementsByClassName("most_dl_no");
-var top_star_name = document.getElementsByClassName("top_star_name");
-var top_star_no = document.getElementsByClassName("top_star_no");
-var top_ten = document.getElementById("popular-components");
+function init()
+{
+	var most_recent_time = document.getElementsByClassName("most_recent_time");
+	var most_recent_name = document.getElementsByClassName("most_recent_name");
+	var most_dl_name = document.getElementsByClassName("most_dl_name");
+	var most_dl_no = document.getElementsByClassName("most_dl_no");
+	var top_star_name = document.getElementsByClassName("top_star_name");
+	var top_star_no = document.getElementsByClassName("top_star_no");
+	var top_ten = document.getElementById("popular-components");
 
-$.getJSON( "http://139.59.93.32/api/datatest/index/", function( data ) {
-  var recent = data.most_recent_components;
-  var star = data.top_starred_components;
-  var dl = data.top_dl_components;
+	$.getJSON( "http://139.59.93.32/api/datatest/index/", function( data ) {
+	  var recent = data.most_recent_components;
+	  var star = data.top_starred_components;
+	  var dl = data.top_dl_components;
 
-  $.each(dl, function(k, v) {
-    most_dl_name[k].innerHTML = (dl[k].name);
-    most_dl_no[k].innerHTML = (dl[k].downloads);
-  });
+	  $.each(dl, function(k, v) {
+	    most_dl_name[k].innerHTML = (dl[k].name);
+	    most_dl_no[k].innerHTML = (dl[k].downloads);
+	  });
 
-  $.each(star, function(k, v) {
-    top_star_name[k].innerHTML = (star[k].name);
-    top_star_no[k].innerHTML = (star[k].stars);
-  });
+	  $.each(star, function(k, v) {
+	    top_star_name[k].innerHTML = (star[k].name);
+	    top_star_no[k].innerHTML = (star[k].stars);
+	  });
 
-  $.each(recent, function(k, v) {
-    most_recent_name[k].innerHTML = (recent[k].name);
-    most_recent_time[k].innerHTML = (recent[k].modified_time.substring(0, 10));
-  });
-});
-setInterval(function() {console.log(window.localStorage.getItem("component"));}, 1000)
-// localStorage.setItem("component", "hurray");
-$.getJSON( "http://139.59.93.32/api/datatest/top/", function( data ) {
-  var components = data.top_components;
+	  $.each(recent, function(k, v) {
+	    most_recent_name[k].innerHTML = (recent[k].name);
+	    most_recent_time[k].innerHTML = (recent[k].modified_time.substring(0, 10));
+	  });
+	});
+	$.getJSON( "http://139.59.93.32/api/datatest/top/", function( data ) {
+	  var components = data.top_components;
 
-  $.each(components, function(k, v) {
-    top_ten.innerHTML += '<a style="text-decoration: none; color: #000;" href="component.html" onclick="setItem(this.id)" id="'+components[k].url_name+'"><div class="component" style="cursor: pointer"><div class="component-img"><img src="'+components[k].icon_url+'"></div><div class="component-main-misc"><div class="component-info-main"><div class="component-name">'+components[k].name+'</div><div class="component-author">Max Franz</div><div class="component-info">'+components[k].short_description+'</div><div class="component-tags"><label>Tags:</label>'+components[k].tags+'</div></div><div class="component-misc"><div class="component-downloads"><span>'+components[k].downloads+'</span><span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span></div><div class="component-stars"><span>'+components[k].stars+'</span><span class="glyphicon glyphicon-star" aria-hidden="true"></span></div><div class="component-modified"><span>'+components[k].modified_time.substring(0, 10)+'</span><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span></div></div></div></div></a>';
-    // var component = document.getElementsByClassName('component')[k];
-  });
-});
+	  $.each(components, function(k, v) {
+	    top_ten.innerHTML += '<a style="text-decoration: none; color: #000;" href="component.html" onclick="setItem(this.id)" id="'+components[k].url_name+'"><div class="component" style="cursor: pointer"><div class="component-img"><img src="'+components[k].icon_url+'"></div><div class="component-main-misc"><div class="component-info-main"><div class="component-name">'+components[k].name+'</div><div class="component-author">Max Franz</div><div class="component-info">'+components[k].short_description+'</div><div class="component-tags"><label>Tags:</label>'+components[k].tags+'</div></div><div class="component-misc"><div class="component-downloads"><span>'+components[k].downloads+'</span><span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span></div><div class="component-stars"><span>'+components[k].stars+'</span><span class="glyphicon glyphicon-star" aria-hidden="true"></span></div><div class="component-modified"><span>'+components[k].modified_time.substring(0, 10)+'</span><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span></div></div></div></div></a>';
+	    // var component = document.getElementsByClassName('component')[k];
+	  });
+	});
+}
+init();
